6d6800881037e191cfd3f7125813626685f30575
sanjeebr <sanjeebr@mindfiresolutions.com>
2016-06-10 19:07:09 +0530

    replaced requre with include and add delete and edit link

--- Profile_App/form/output.php
+++ Profile_App/form/output.php
@@ -1,8 +1,15 @@
 <?php
-include_once("php/mysql_connect_db.php");
-$sql_query="SELECT  employee.id as emp_id,employee.first_name as first_name, employee.middle_name as middle_name, employee.last_name as last_name, employee.date_of_birth as date_of_birth, employee.prefix as prefix, employee.photo as photo, employee.note as note, employee.gender as gender, employee.marital_status as marital, employee.communication as communication, employee.employment as employment, employee.employer as employer,residence.street as r_street,residence.city as r_city,residence.pin_no as r_pin,residence.phone as r_phone,residence.fax as r_fax,office.street as o_street,office.city as o_city,office.pin_no as o_pin,office.phone as o_phone,office.fax as o_fax FROM employee left join address as residence on employee.id=residence.employee_id and residence.type='residence' left join address as office on employee.id=office.employee_id and office.type='office'";
+
+require_once("config/mysql_connect_db.php");
+
+$sql_query="SELECT  employee.id as emp_id,employee.first_name as first_name, employee.middle_name as middle_name, employee.last_name as last_name, employee.date_of_birth as date_of_birth, employee.prefix as prefix, employee.photo as photo, employee.note as note, employee.gender as gender, employee.marital_status as marital, employee.communication as communication, employee.employment as employment, employee.employer as employer,residence.street as r_street,residence.city as r_city,residence.pin_no as r_pin,residence.phone as r_phone,residence.fax as r_fax,office.street as o_street,office.city as o_city,office.pin_no as o_pin,office.phone as o_phone,office.fax as o_fax
+  FROM employee 
+  left join address as residence on employee.id=residence.employee_id and residence.type='residence'
+  left join address as office on employee.id=office.employee_id and office.type='office'";
      
   $result=mysqli_query($conn,$sql_query);
+  $num_rows = mysqli_num_rows($result);
+
 ?>
 
 
@@ -35,29 +42,33 @@ $sql_query="SELECT  employee.id as emp_id,employee.first_name as first_name, emp
     </div>
   </div>
 </nav>
-    <div class="container-fluid">
+<?php if($num_rows > 0) {?>
+    <div  class="table-responsive">
+
   <h2>Employee Details</h2>           
-  <table class="table table-striped success">
+  <table class="table table-striped">
     <thead>
       <tr>
-        <th>Emp ID</th>
-        <th>Name</th>
-        <th>Gender</th>
-        <th>Date of Birth</th>
-        <th>Marital Status</th>
-        <th>Residence Address</th>
-        <th>Office Address</th>
-        <th>Employment</th>
-        <th>Employer</th>
-        <th>Extra Note</th>
-        <th>Communication Medium</th>
+        <th><h4>Emp ID</h4></th>
+        <th><h4>Name</h4></th>
+        <th><h4>Gender</h4></th>
+        <th><h4>Date of Birth</h4></th>
+        <th><h4>Marital Status</h4></th>
+        <th><h4>Residence Address</h4></th>
+        <th><h4>Office Address</h4></th>
+        <th><h4>Employment</h4></th>
+        <th><h4>Employer</h4></th>
+        <th><h4>Extra Note</h4></th>
+        <th><h4>Communication Medium</h4></th>
+        <th><h4>Edit</h4></th>
+        <th><h4>Delete</h4></th>
       </tr>
     </thead>
     <tbody>
     <?php while($row=mysqli_fetch_assoc($result)){ ?>
       <tr>
-        <td><h3><?php echo $row['emp_id']; ?></h3></td>
-        <td><?php echo $row['prefix'].' '.$row['first_name'].' '.$row['middle_name'].' '.$row['Last_name']; ?></td>
+        <td><?php echo $row['emp_id']; ?></td>
+        <td><?php echo $row['prefix'].' '.$row['first_name'].' '.$row['middle_name'].' '.$row['last_name']; ?></td>
         <td><?php echo $row['gender']; ?></td>
         <td><?php echo $row['date_of_birth']; ?></td>
         <td><?php echo $row['marital']; ?></td>
@@ -67,11 +78,21 @@ $sql_query="SELECT  employee.id as emp_id,employee.first_name as first_name, emp
         <td><?php echo $row['employer']; ?></td>
         <td><?php echo $row['note']; ?></td>
         <td><?php echo $row['communication']; ?></td>
+        <td><a><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a></td>
+        <td><a onclick="return confirm('The row will be deleted')"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></a></td>
       </tr>
      <?php   } ?>
     </tbody>
   </table>
 </div>
+  <?php } else {?>
+      <div class="container">
+  <h2>NO DATA  IS PRESENT TO SHOW </h2>           
+</div>
+
+
+
+    <?php }?>
 </div> 
 </body>
 </html>
