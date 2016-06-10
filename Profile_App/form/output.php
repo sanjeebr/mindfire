03d8e40e500e6f52c9757b9054b7bfdb59431fe5
sanjeebr <sanjeebr@mindfiresolutions.com>
2016-06-10 15:21:36 +0530

    Display of data completed

--- Profile_App/form/output.php
+++ Profile_App/form/output.php
@@ -1,23 +1,8 @@
-<?php 
-$prefix=$_POST['prefix'];
-$first_name=$_POST['first_name'];
-$middle_name=$_POST['middle_name'];
-$last_name=$_POST['last_name'];
-$gender=$_POST['gender'];
-$date_of_birth=$_POST['date_of_birth'];
-$marital=$_POST['marital'];
-$r_street=$_POST['r_street'];
-$r_city=$_POST['r_city'];
-$r_state=$_POST['r_state'];
-$r_pin=$_POST['r_pin'];
-$r_phone=$_POST['r_phone'];
-$o_fax=$_POST['r_fax'];
-$o_street=$_POST['o_street'];
-$o_city=$_POST['o_city'];
-$o_state=$_POST['o_state'];
-$o_pin=$_POST['o_pin'];
-$o_phone=$_POST['o_phone'];
-$o_fax=$_POST['o_fax'];
+<?php
+include_once("php/mysql_connect_db.php");
+$sql_query="SELECT  employee.id as emp_id,employee.first_name as first_name, employee.middle_name as middle_name, employee.last_name as last_name, employee.date_of_birth as date_of_birth, employee.prefix as prefix, employee.photo as photo, employee.note as note, employee.gender as gender, employee.marital_status as marital, employee.communication as communication, employee.employment as employment, employee.employer as employer,residence.street as r_street,residence.city as r_city,residence.pin_no as r_pin,residence.phone as r_phone,residence.fax as r_fax,office.street as o_street,office.city as o_city,office.pin_no as o_pin,office.phone as o_phone,office.fax as o_fax FROM employee left join address as residence on employee.id=residence.employee_id and residence.type='residence' left join address as office on employee.id=office.employee_id and office.type='office'";
+     
+  $result=mysqli_query($conn,$sql_query);
 ?>
 
 
@@ -26,33 +11,47 @@ $o_fax=$_POST['o_fax'];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-    <title>Registration</title>
+    <title>Employee Details</title>
     <link rel="stylesheet" href="css/bootstrap.min.css" />
     <link rel="stylesheet" href="css/output.css"  />
 </head>
 <body>
-    <div class="container">
-         <?php echo "<h2>Personal Info:</h2><br>
-         <h3>Name: $prefix $first_name $middle_name $last_name<br>
-         Gender:$gender<br>
-         Date of Birth:$date_of_birth<br>
-         Marital Status:$marital<br></h3>
-         <h2>Residence Address:</h2>
-         <h3>$r_street,$r_city,$r_state</h3>
-         <h3>Pin no:$r_pin</h3>
-         <h3>Phone no:$r_phone</h3>
-         <h3>Fax no:$r_fax</h3>
-         <h2>Office Address:</h2>
-         <h3>$o_street,$o_city,$o_state</h3>
-         <h3>Pin no:$o_pin</h3>
-         <h3>Phone no:$o_phone</h3>
-         <h3>Fax no:$o_fax</h3>
-
-         "; 
-
-
-
-         ?> 
-    </div> 
+    <div class="container-fluid">
+  <h2>Employee Details</h2>           
+  <table class="table table-striped">
+    <thead>
+      <tr>
+        <th>Emp ID</th>
+        <th>Name</th>
+        <th>Gender</th>
+        <th>Date of Birth</th>
+        <th>Marital Status</th>
+        <th>Residence Address</th>
+        <th>Office Address</th>
+        <th>Employment</th>
+        <th>Employer</th>
+        <th>Extra Note</th>
+        <th>Communication Medium</th>
+      </tr>
+    </thead>
+    <tbody>
+    <?php while($row=mysqli_fetch_assoc($result)){ ?>
+      <tr>
+        <td><h3><?php echo $row['emp_id']; ?></h3></td>
+        <td><?php echo $row['prefix'].' '.$row['first_name'].' '.$row['middle_name'].' '.$row['Last_name']; ?></td>
+        <td><?php echo $row['gender']; ?></td>
+        <td><?php echo $row['date_of_birth']; ?></td>
+        <td><?php echo $row['marital']; ?></td>
+        <td><?php echo $row['r_street'].' ,'.$row['r_city'].','.$row['r_state'].'<br>'.'Pin no:'.$row['r_pin'].'<br>'.'Phone no:'.$row['r_phone'].'<br>'.'Fax no:'.$row['r_fax']; ?></td>
+        <td><?php echo $row['o_street'].' ,'.$row['o_city'].','.$row['o_state'].'<br>'.'Pin no:'.$row['o_pin'].'<br>'.'Phone no:'.$row['o_phone'].'<br>'.'Fax no:'.$row['o_fax']; ?></td>
+        <td><?php echo $row['employment']; ?></td>
+        <td><?php echo $row['employer']; ?></td>
+        <td><?php echo $row['note']; ?></td>
+        <td><?php echo $row['communication']; ?></td>
+      </tr>
+     <?php   } ?>
+    </tbody>
+  </table>
+</div> 
 </body>
 </html>
