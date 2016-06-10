173322569b163e364b970bbb28d45d5b47626f24
sanjeebr <sanjeebr@mindfiresolutions.com>
2016-06-10 15:43:53 +0530

    Added navigation bar

--- Profile_App/form/form.php
+++ Profile_App/form/form.php
@@ -56,7 +56,25 @@ if (isset($_POST['submit'])) {
     <link rel="stylesheet" href="css/form.css"  />
 </head>
 <body>
-    <div class="container-fluid">
+<div class="container-fluid" id="container_1">
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#">Sanjeeb</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="">Registration</a></li>
+        <li ><a href="output.php">Employee Details</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
         <div class="container">
             <h1>Registration Form</h1>
             <br>
