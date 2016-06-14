f96c08fd0095861cd8c3bcc2deee9ebd7fc63266
sanjeebr <sanjeebr@mindfiresolutions.com>
2016-06-14 14:50:28 +0530

    Upload Functionality Implemented.

--- Profile_App/form/delete.php
+++ Profile_App/form/delete.php
@@ -1,13 +1,13 @@
 <?php
 require_once("config/mysql_connect_db.php");
-
 if (isset($_GET['emp_id'])) {
-    
-    $sql='DELETE FROM address WHERE employee_id ='. $_GET['emp_id'];
+    $emp_id = stripslashes($_GET['emp_id']);
+    $photo = 'profile_pic/'.stripslashes($_GET['photo']);
+    $sql='DELETE FROM address WHERE employee_id ='. $emp_id;
     mysqli_query($conn,$sql);
-
-	$sql='DELETE FROM employee WHERE id ='.$_GET['emp_id'];
+	$sql='DELETE FROM employee WHERE id ='.$emp_id;
 	mysqli_query($conn,$sql);
+	unlink($photo);
        
 }
 header("Location: output.php");
